87092d1d3aa65e376089107adaefd638ae96d63c
gowrisettyrk <gowrisettyramakrishna3@gmail.com>
2022-11-16 11:41:44 +0530

    Added sample Assert test cases

--- test/com/rk/junit/MyMathTest.java
+++ test/com/rk/junit/MyMathTest.java
@@ -5,15 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
+	
+	private MyMath math = new MyMath();
 
 	@Test
-	void test() {
-		
-		int[] num = {1,2,3};
-		MyMath math = new MyMath();
-		int result = math.calculateSum(num);
-		int expResult = 6;
-		assertEquals(expResult,result);
+	void calcucalteSum_ThreeMemberArray() {
+		assertEquals(6,math.calculateSum(new int[] {1,2,3}));
+	}
+	
+	@Test
+	void calculateSum_NullArray() {
+		assertEquals(0,math.calculateSum(new int[] {}));
 	}
 
 }
